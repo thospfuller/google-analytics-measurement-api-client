19d40db9623637c9685944ddfcc96c29a430a588
thospfuller <thomas.fuller@coherentlogic.com>
2016-09-13 13:11:00 -0400

    Changes since the return type of the doPost method has changed and added a new test.

--- gama-client-core-it/src/test/java/com/coherentlogic/gama/client/core/builders/QueryBuilderTest.java
+++ gama-client-core-it/src/test/java/com/coherentlogic/gama/client/core/builders/QueryBuilderTest.java
@@ -1,11 +1,21 @@
 package com.coherentlogic.gama.client.core.builders;
 
-import static org.junit.Assert.assertEquals;
+import static org.mockito.Matchers.any;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
+
+import java.net.URI;
 
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.client.RestTemplate;
+
+import com.coherentlogic.gama.client.core.exceptions.PostFailedException;
 
 /**
  * Integration test for the {@link QueryBuilder} class.
@@ -23,7 +33,7 @@ public class QueryBuilderTest {
 
     @Before
     public void setUp() throws Exception {
-        queryBuilder = new QueryBuilder(new RestTemplate ());
+        queryBuilder = new QueryBuilder();
     }
 
     @After
@@ -34,7 +44,7 @@ public class QueryBuilderTest {
     @Test
     public void testSuccessfulGAPost () {
 
-        String result = queryBuilder
+        ResponseEntity<?> result = queryBuilder
             .withV1()
             .withTid(googleAnalyticsUAID)
             .withCIDAsRandomUUID()
@@ -49,4 +59,31 @@ public class QueryBuilderTest {
         // We'll need to manually check that the data has, in fact, been recevied by GA.
     }
 
+    /**
+     * This is not an integration test however it does belong here. The Google Analytics Measurement API will accept
+     * just about any call made, returning a 200 (OK) status code, even when the data sent is completely wrong. We need
+     * to make sure than anything aside from OK results in an exception being thrown.
+     */
+    @Test(expected=PostFailedException.class)
+    public void testUnsuccessfulGAPost () {
+
+        RestTemplate mockRestTemplate = mock(RestTemplate.class);
+
+        ResponseEntity responseEntity = mock (ResponseEntity.class);
+
+        when (
+            mockRestTemplate.exchange(
+                any(URI.class),
+                any(HttpMethod.class),
+                any(HttpEntity.class),
+                any(Class.class)
+            )
+        ).thenReturn (responseEntity);
+
+        when (responseEntity.getStatusCode()).thenReturn(HttpStatus.CONFLICT);
+
+        ResponseEntity<?> result = new QueryBuilder (mockRestTemplate).doPost();
+    }
+
+    
 }
